3c25d8582d7fe15f30731654ae03d127f57fa66e
skatpgusskat <skatpgusskat@naver.com>
2018-07-14 22:36:45 +0900

    Add volume slider

--- public/index.js
+++ public/index.js
@@ -1,5 +1,18 @@
-const airhorn = document.getElementById("airhorn");
-airhorn.volume = 0.2;
+const airhorn = document.getElementById('airhorn');
+const volumeSlider = document.getElementById('volume');
+
+function init() {
+  const defaultVolume = localStorage.getItem('volume') || 0.2
+  airhorn.volume = defaultVolume;
+  volumeSlider.value = defaultVolume;
+}
+
+volumeSlider.oninput = function () {
+  const volume = this.value;
+
+  airhorn.volume = volume;
+  localStorage.setItem('volume', volume);
+};
 
 function onF6() {
   console.log('f6');
@@ -10,3 +23,5 @@ function onF6() {
 
   airhorn.play();
 }
+
+init();
